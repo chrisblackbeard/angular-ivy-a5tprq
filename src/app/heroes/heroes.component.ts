173cfc0d8e171f582c858ae0651ba0d05be0e9b8
chrisblackbeard <chrisblackbeardjensen@gmail.com>
2020-08-07 14:37:55 +0800

    2 files modified, 1 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from "../hero"
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  heroes='winston'
+  hero: Hero = {
+    id: 0,
+    name: 'Winston'
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
